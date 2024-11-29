eb397ec25a9c9060c27a3855d5ca56fbeb82060e
Sutrisno_tris <sutris.tris032@gmail.com>
2024-11-29 12:39:40 +0700

    Laravel Routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return "hello Word";
+});
+
+Route::get('/hello', function () {
+    return "Hello";
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return "Hello " . $name . "!";
+});
+
+Route::fallback(function () {
+    return "404 Notfound Page";
 });
