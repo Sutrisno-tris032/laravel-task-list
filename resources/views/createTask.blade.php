7287766586a181d62be4f7112b6321e28376af8d
Sutrisno_tris <sutris.tris032@gmail.com>
2024-12-16 21:29:30 +0800

    post the data

---
+++ resources/views/createTask.blade.php
@@ -0,0 +1,44 @@
+@extends('layout.app')
+
+@section('title', 'Create Task')
+
+@section('styles')
+    <style>
+        .error_message {
+            color: red;
+            font-size: 0.8rem;
+        }
+    </style>
+@endsection
+
+@section('content')
+    <form method="POST" action="{{ route('tasks.store') }}">
+        @csrf
+        <div>
+            <label for="title">Title</label>
+            <input type="text" name="title" />
+            @error('title')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <label for="description">Description</label>
+            <textarea name="description" id="" cols="30" rows="5"></textarea>
+            @error('description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div>
+            <label for="long_description">Description</label>
+            <textarea name="long_description" id="" cols="30" rows="10"></textarea>
+            @error('long_description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Task as ModelsTask;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -12,11 +13,39 @@ Route::get('task/{task_id}', function ($id) {
   ]);
 })->name('tasks.show');
 
-
 Route::get('/tasks', function () {
   return view('index', [
-    'tasks' => ModelsTask::latest()->where('completed', true)->get()
+    'tasks' => ModelsTask::latest()->get()
   ]);
 })->name('tasks.list');
 
+Route::post('/task/store', function (Request $request) {
+   $data = $request->validate([
+    'title' => 'required|max:255',
+    'description' => 'required',
+    'long_description' => 'required'
+   ]);
+   
+   $task = new ModelsTask;
+   $task->title = $data['title'];
+   $task->description = $data['description'];
+   $task->long_description = $data['long_description'];
+   $task->save();
+   
+   return redirect()->route('tasks.list');
 
+})->name('tasks.store');
+
+Route::get('task/{task_id}/edit', function ($id) {
+  return view('editTask', [
+    'task' => ModelsTask::findOrFail($id)
+  ]);
+})->name('tasks.edit');
+
+Route::get('/tasks/create', function () {
+  return view('createTask');
+})->name('tasks.create');
+
+
+
+// retrun view only 
